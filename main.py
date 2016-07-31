4a553eaea87833a5075cc598dcf9f475c5cc5251
Slawek Kolodziej <hfrntt@gmail.com>
2016-07-31 18:28:36 +0200

    Add basic code generator commands

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 """
 Tool for controlling LG air conditioner from Raspberry PI
 """
-
 import wiringpi as pi
+from lg import Remote
 
 pi.wiringPiSetup()
 pi.pinMode(0, pi.GPIO.OUTPUT)
 pi.digitalWrite(0, pi.GPIO.HIGH)
+
+REMOTE = Remote()
+
+REMOTE.set_mode("COOLING", "1", 18, "ON")
+
+print REMOTE.debug()
+
+# serial = pi.serialOpen('/dev/ttyAMA0', 9600)
+
+# pi.serialClose(serial)
