0750453add8d8920555f35860675e3903daa0073
Slawek Kolodziej <hfrntt@gmail.com>
2016-08-02 02:07:53 +0200

    Translate remote codes to PWM signals

--- main.py
+++ main.py
@@ -1,19 +1,46 @@
 """
 Tool for controlling LG air conditioner from Raspberry PI
 """
-import wiringpi as pi
-from lg import Remote
+import pigpio
+PIN = 18
+pi = pigpio.pi()
+pi.set_mode(PIN, pigpio.OUTPUT)
+# pi.hardware_clock(PIN, 38000)
 
-pi.wiringPiSetup()
-pi.pinMode(0, pi.GPIO.OUTPUT)
-pi.digitalWrite(0, pi.GPIO.HIGH)
+pi.write(PIN, 0)
 
+
+from lg import Remote, BUFFER_SIZE
 REMOTE = Remote()
-
 REMOTE.set_mode("COOLING", "1", 18, "ON")
 
-print REMOTE.debug()
+def space(time):
+    """
+    """
+    pi.hardware_PWM(PIN, 38000, 0)
+    delay(time)
 
-# serial = pi.serialOpen('/dev/ttyAMA0', 9600)
+def mark(time):
+    """
+    """
+    pi.hardware_PWM(PIN, 38000, 1000000)
+    delay(time)
 
-# pi.serialClose(serial)
+def delay(usec):
+    if usec > 4:
+        start = pi.get_current_tick()
+        end = start + usec - 4
+
+        if end < start:
+            while pi.get_current_tick() > start:
+                pass
+        while pi.get_current_tick() < end:
+            pass
+
+for n in range(0, BUFFER_SIZE):
+    if n & 1:
+        space(REMOTE.codes[n])
+    else:
+        mark(REMOTE.codes[n])
+
+pi.write(PIN, 0)
